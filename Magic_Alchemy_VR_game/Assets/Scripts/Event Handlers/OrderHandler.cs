56c243d0e0719d52abc50fa7a21db20beab43494
Виталий Дмитриев <dmitriz_vitaliy@mail.ru>
2022-12-06 14:24:25 +0500

    Добавил процесс выполнения заказов
    Расставил на сцене необходимые компоненты для выполнения заказов: сундук, платформу и кнопку

--- Magic_Alchemy_VR_game/Assets/Scripts/Event Handlers/OrderHandler.cs
+++ Magic_Alchemy_VR_game/Assets/Scripts/Event Handlers/OrderHandler.cs
@@ -17,20 +17,20 @@ namespace EventHandlers
         private void OnEnable()
         {
             Box.GetCurrentOrder += GetCurrentOrder;
-            SendingButton.OrderDone += _order.Generate;
+            SendingButton.OrderDone += FinishOrder;
         }
 
         private void OnDisable()
         {
             Box.GetCurrentOrder -= GetCurrentOrder;
-            SendingButton.OrderDone -= _order.Generate;
+            SendingButton.OrderDone -= FinishOrder;
         }
 
         private KeyValuePair<ElementType, int> GetCurrentOrder() => _order.CurrentOrder;
 
         private void FinishOrder()
         {
-
+            _order.GetCash();
             _order.Generate();
         }
     }
