679eff473073c34ff7d19e2a6e2a4d7a25227225
Виталий Дмитриев <dmitriz_vitaliy@mail.ru>
2023-01-11 22:05:49 +0500

    Исправил текст в обучении

--- Magic_Alchemy_VR_game/Assets/Scripts/Gameplay/NotificationS/Tutorial.cs
+++ Magic_Alchemy_VR_game/Assets/Scripts/Gameplay/NotificationS/Tutorial.cs
@@ -59,30 +59,30 @@ namespace Gameplay.Notifications
             _notificationSystem = GetComponent<NotificationSystem>();
 
             var tutorialTextsPart1 = new Queue<(string, bool)>();
-            tutorialTextsPart1.Enqueue(("������! ��� ���� �Magic Alchemy�.", true));
-            tutorialTextsPart1.Enqueue(("���� ���� ��������� ������, ����� �������� ������ �� ������� ������������ �����.", true));
-            tutorialTextsPart1.Enqueue(("����� ��������� ����������� ������ �����!", true));
-            tutorialTextsPart1.Enqueue(("��� ������ ������ ����������� �������� � ������ ��� ������ ����������.", true));
-            tutorialTextsPart1.Enqueue(("P.S. ������� � ����� ���� ���������� ���������� ���������� ��������.", false));
+            tutorialTextsPart1.Enqueue(("Привет! Это игра “Magic Alchemy”.", true));
+            tutorialTextsPart1.Enqueue(("Тебе надо выполнять заказы, чтобы накопить монеты на покупку философского камня.", true));
+            tutorialTextsPart1.Enqueue(("Давай попробуем реализовать первый заказ!", true));
+            tutorialTextsPart1.Enqueue(("Для начала возьми необходимые элементы и создай своё первое соединение.", true));
+            tutorialTextsPart1.Enqueue(("P.S. Рецепты в нашей игре аналогичны мобильному приложению “Алхимик”.", false));
 
             var tutorialTextsPart2 = new Queue<(string, bool)>();
-            tutorialTextsPart2.Enqueue(("�������!", true));
-            tutorialTextsPart2.Enqueue(("����� � ���, ��� ���� �� ����������� �������� �������������� ����������, �� ��������� �����!", true));
-            tutorialTextsPart2.Enqueue(("��������! �� ����� ����� �����.", true));
-            tutorialTextsPart2.Enqueue(("�� ������ �������� ���������� � ������, ����� ��������� ������������ ���������� ������.", false));
+            tutorialTextsPart2.Enqueue(("Молодец!", true));
+            tutorialTextsPart2.Enqueue(("Помни о том, что если ты попытаешься получить несуществующее соединение, то произойдёт взрыв!", true));
+            tutorialTextsPart2.Enqueue(("Посмотри! На доске новый заказ.", true));
+            tutorialTextsPart2.Enqueue(("Ты можешь положить соединение в сундук, чтобы проверить правильность выполнения заказа.", false));
 
             var tutorialTextsPart3 = new Queue<(string, bool)>();
-            tutorialTextsPart3.Enqueue(("���-�� ����� �� ���. ��������, ���������� ����������� ������ ����������.", false));
+            tutorialTextsPart3.Enqueue(("Что-то пошло не так. Возможно, необходимо реализовать другое соединение.", false));
 
             var tutorialTextsPart4 = new Queue<(string, bool)>();
-            tutorialTextsPart4.Enqueue(("���! ������ ����� �������� �������� �����������.", true));
-            tutorialTextsPart4.Enqueue(("� ������ ����� �� ������� ������ ��� �������� ������.", false));
+            tutorialTextsPart4.Enqueue(("Ура! Первый заказ выполнен согласно требованиям.", true));
+            tutorialTextsPart4.Enqueue(("А теперь нажми на красную кнопку для отправки заказа.", false));
 
             var tutorialTextsPart5 = new Queue<(string, bool)>();
-            tutorialTextsPart5.Enqueue(("��� � �������� ������ �����. ��������� � ��� �� ����. �����!", true));
+            tutorialTextsPart5.Enqueue(("Вот и выполнен первый заказ. Продолжай в том же духе. Удачи!", true));
 
             var tutorialTextsPart6 = new Queue<(string, bool)>();
-            tutorialTextsPart6.Enqueue(("�����������! ���� ����������. ���������� �� ����������� ����-������ �Magic Alchemy�!", true));
+            tutorialTextsPart6.Enqueue(("Поздравляем! Цель достигнута. Благодарим за прохождение демо-версии “Magic Alchemy”!", true));
 
             _tutorialTexts = new List<Queue<(string, bool)>>
             {
